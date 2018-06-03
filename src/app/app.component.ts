7f4a9e6e7a66def9595054e080b61a6c7c2d2dc5
sgattani <saurabh.gattani@gmail.com>
2018-06-03 10:54:45 +0530

    added launch info list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,30 @@ export class AppComponent implements OnInit {
  
    title = 'app works!';
    launchpads; launches;
+   selectedLaunchPad :any;
+   selectedLaunch: any;
  
    constructor(private launchPadService: LaunchPadService,
 			   private launchInfoService: LaunchInfoService) {
+				   this.selectedLaunchPad = 0;
+				   this.selectedLaunch = 0;
    }
+   
+  selectLaunchPad(item) {
+      this.selectedLaunchPad = item; 
+  };
+  
+  isActiveLaunchPad(item) {
+      return this.selectedLaunchPad === item;
+  };
+  
+  selectLaunch(item) {
+      this.selectedLaunch = item; 
+  };
+  
+  isActiveLaunch(item) {
+      return this.selectedLaunch === item;
+  };
  
    ngOnInit() {
       this.launchPadService.getLaunchPads().subscribe(p=>this.launchpads = p);
